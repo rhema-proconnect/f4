bf5ede217d8eaf9084e3265272b71f142911a6ff
Timothée <kadayitim@gmail.com>
2024-12-29 12:50:57 +0000

    cards-2

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // import "bootstrap/dist/css/bootstrap.min.css";
-import Cards from "react-credit-cards";
-import "react-credit-cards/es/styles-compiled.css";
+import Cards from "react-credit-cards-2";
+import "react-credit-cards-2/es/styles-compiled.css";
 import styles from "./styles.css";
 import { Box, TextField } from "@mui/material";
 import Card from '@mui/joy/Card';
